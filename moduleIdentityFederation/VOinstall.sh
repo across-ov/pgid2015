894a8abf1f4cc1efdcc75881ac837aa729925101
Edelberto Franco Silva <edeunix@gmail.com>
2015-07-23 18:32:25 -0300

    all folders

---
+++ moduleIdentityFederation/VOinstall.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo "========================="
+echo " Starting VO Install"
+echo "========================="
+echo "========================="
+echo "V0.1"
+echo "PGID 2015"
+echo "========================="
+
+
+# name of pkgs
+$apache = "apache2"
+$shib = "shibboleth"
+$modshib = "libapache2-mod-shib2"
+
+echo "Updating package list"
+apt-get update  # To get the latest package lists
+
+echo "Download and install Apache"
+apt-get install $apache -y
+echo "Download and install Shibboleth 2.X"
+apt-get install $apache -y
+echo "Download and enabling mod_shib (Apache)"
+apt-get install $modshib -y
+a2enmod shib2
+
+echo "Please, inform the file (with full path) of metadata file:"
+#
