731e93c608eeea437c3685fafae0364e034434b3
Edelberto Franco <edeunix@gmail.com>
2015-08-05 15:19:09 -0300

    05 aug

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-pgid2015
+PGID2015
+========
+Author: Edelberto Franco Silva
+=======
+Version: 0.1
+=======
+About (restricted acess):
+https://wiki.rnp.br/display/comitetgi/PGId+2015+-+Propostas+Selecionadas
+
--- moduleIdentityFederation/VOinstall.sh
+++ moduleIdentityFederation/VOinstall.sh
@@ -12,20 +12,47 @@ echo "Info: This wizard will guide you to configure your Identity Federation Mod
 echo "Run as root or using sudo."
 
 # name of pkgs
-$apache = "apache2"
-$shib = "shibboleth"
-$modshib = "libapache2-mod-shib2"
+apache="apache2"
+shib="shibboleth"
+modshib="libapache2-mod-shib2"
 
-echo "Updating package list"
-apt-get update  # To get the latest package lists
+read -p "VO name (e.g. across): " name
+read -p "IP address (e.g. 200.193.144.3): " ip
+read -p "FQDN address (e.g. across.fibre.org.br): " fqdn
 
-echo "Download and install Apache"
-apt-get install $apache -y
-echo "Download and install Shibboleth 2.X"
-apt-get install $apache -y
-echo "Download and enabling mod_shib (Apache)"
-apt-get install $modshib -y
-a2enmod shib2
+#echo $name $ip $fqdn
 
-echo "Please, inform the file (with full path) of metadata file:"
+echo "Updating package list.."
+apt-get -qq update  # To get the latest package lists
+
+# check if packages was installed
+# Apache2
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $apache|grep "install ok installed")
+echo Checking for somelib: $apache was installed
+if [ "" == "$PKG_OK" ]; then
+  echo "No $apache ... Setting up $apache ..."
+  echo "Download and install Apache"
+  sudo apt-get --force-yes --yes -qq install $apache
+fi
+ 
+# Shibolleth 2.X
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $shib|grep "install ok installed")
+echo Checking for somelib: $shib was installed
+if [ "" == "$PKG_OK" ]; then
+  echo "No $shib ... Setting up $shib ..."
+  echo "Download and install Apache"
+  sudo apt-get --force-yes --yes -qq install $shib
+fi
+ 
+# mod_shib
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $modshib|grep "install ok installed")
+echo Checking for somelib: $modshib was installed
+if [ "" == "$PKG_OK" ]; then
+  echo "No $modshib ... Setting up $modshib ..."
+  echo "Download and install Apache"
+  sudo apt-get --force-yes --yes -qq install $modshib
+fi
+ 
+read -p "Please, inform the file (with full path) of metadata file: " metaov
+echo $metaov
 #
