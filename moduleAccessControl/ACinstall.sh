055904923c5b77fb46a132c1b92a79a849951fb3
Edelberto Franco Silva <edeunix@gmail.com>
2015-09-30 18:40:44 -0300

    all folders

---
+++ moduleAccessControl/ACinstall.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "========================="
+echo " Starting Access Control 
+	Configuration"
+echo "========================="
+echo "========================="
+echo "V0.1"
+echo "PGID 2015"
+echo "========================="
+echo "========================="
+echo "Info: This wizard will guide you to configure"
+echo "your Access Control Module"
+
+echo "=========================================================
+echo "Please, complete all requests with non-capital letters."
+echo "***********************************"
+echo "***********************************"
+echo "Warning! Run as root or using sudo."
+echo "***********************************"
+echo "***********************************"
+
